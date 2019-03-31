0575688535061e17d3e86babd2fd69d62ac2a9f8
Suraj <suraj.kumar@pnnl.gov>
2019-03-30 22:36:40 -0700

    Results for batch experiments

---
+++ ExperimentalData/scripts/inverse_ratio_to_optimal_batch-best.R
@@ -0,0 +1,25 @@
+#!/usr/bin/env Rscript
+inverse_ratio_to_optimal_best_func <-function(file_name)
+{
+library(ggplot2)
+library(reshape2)
+dftest<-read.table(file_name, header=T)
+dftest[,2:ncol(dftest)]<-dftest[,2:ncol(dftest)]/dftest[,6]
+
+dftest[,c(2:6)]<-NULL
+dftest$static_best<-do.call('pmin', c(dftest[,3:7]))
+dftest$dynamic_best<-do.call('pmin', c(dftest[,8:10]))
+dftest$static_dynamic_best<-do.call('pmin', c(dftest[,11:13]))
+dftest<-dftest[,c(1,2,14:16)]
+dftestMelted<-melt(dftest, "capacity")
+names(dftestMelted)<-c("capacity", "Heuristic", "ratio_to_optimal")
+p<-ggplot(dftestMelted, aes(x=capacity, y=1/ratio_to_optimal, color=Heuristic))  + geom_smooth()
+p<-p + ylab("inverse(ratio_to_optimal)") + theme(plot.title = element_text(hjust = 0.5)) +  ggtitle("Performance With Best of all categories")
+return (p)
+}
+p<-inverse_ratio_to_optimal_best_func('results_hf_batch.txt')
+ggsave(file="inverse_ratio_to_optimal_hf_batch-best.pdf", p, width=23.6, height=13.4)
+
+p<-inverse_ratio_to_optimal_best_func('results_ccsd_batch.txt')
+ggsave(file="inverse_ratio_to_optimal_ccsd_batch-best.pdf", p, width=23.6, height=13.4)
+
